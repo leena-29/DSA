500ba580278a6d2d74b8a1622306ca23464c6d39
LEENA <131523797+leena-29@users.noreply.github.com>
2025-04-09 22:46:47 +0530

    Add files via upload

---
+++ count all palindrome subsequence in a given string.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    int arr[] = {29, 10, 14, 37, 13};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
