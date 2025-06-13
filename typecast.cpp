1cd0c4f0c62540ec2b0552b466730c8095805e59
LEENA <131523797+leena-29@users.noreply.github.com>
2025-06-13 23:50:41 +0530

    Add files via upload

---
+++ typecast.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a=65.01;
+float c;char b;
+cout<<"the typecatsed value is : "<<(float)a<<"\n";
+cout<<"the typecatsed value is : "<<(char)a;
+}
